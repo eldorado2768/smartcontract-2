2c0490c6877c8457020792e8c02f88a591faa4a1
David Brotman <david@123websitebiz.com>
2025-08-09 00:16:51 -0700

    Update FlashLoanArbitrage.sol

--- FlashLoanArbitrage.sol
+++ FlashLoanArbitrage.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+//Note: environment is injected-provider Metamask
+
 // Corrected import paths
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -204,3 +206,4 @@ contract FlashLoanArbitrage is IFlashLoanSimpleReceiver {
         }
     }
 }
+
